1668fc151af580c2426f630c9cd9a728a681aeaa
Mert Ağralı <100879484+mertagralii@users.noreply.github.com>
2023-12-16 21:47:01 +0300

    Add project files.

---
+++ KelimeOyunuProjesi/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace KelimeOyunuProjesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.BtnBasla = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.BtnTahminEt = new System.Windows.Forms.Button();
+            this.BtnKelimeAl = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BtnBasla
+            // 
+            this.BtnBasla.Location = new System.Drawing.Point(7, 12);
+            this.BtnBasla.Name = "BtnBasla";
+            this.BtnBasla.Size = new System.Drawing.Size(151, 63);
+            this.BtnBasla.TabIndex = 0;
+            this.BtnBasla.Text = "BAŞLA";
+            this.BtnBasla.UseVisualStyleBackColor = true;
+            this.BtnBasla.Click += new System.EventHandler(this.BtnBasla_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(210, 495);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(286, 30);
+            this.textBox1.TabIndex = 2;
+            // 
+            // BtnTahminEt
+            // 
+            this.BtnTahminEt.Location = new System.Drawing.Point(931, 412);
+            this.BtnTahminEt.Name = "BtnTahminEt";
+            this.BtnTahminEt.Size = new System.Drawing.Size(151, 63);
+            this.BtnTahminEt.TabIndex = 3;
+            this.BtnTahminEt.Text = "Tahmin Et";
+            this.BtnTahminEt.UseVisualStyleBackColor = true;
+            this.BtnTahminEt.Click += new System.EventHandler(this.BtnTahminEt_Click);
+            // 
+            // BtnKelimeAl
+            // 
+            this.BtnKelimeAl.Location = new System.Drawing.Point(931, 495);
+            this.BtnKelimeAl.Name = "BtnKelimeAl";
+            this.BtnKelimeAl.Size = new System.Drawing.Size(151, 63);
+            this.BtnKelimeAl.TabIndex = 4;
+            this.BtnKelimeAl.Text = "Kelime Al";
+            this.BtnKelimeAl.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(1143, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(54, 29);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Palatino Linotype", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(420, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(294, 55);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Kelime Oyunu";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(12, 487);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(194, 38);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Tahmin Et :";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(-2, 131);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1199, 256);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(525, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1200, 667);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.BtnKelimeAl);
+            this.Controls.Add(this.BtnTahminEt);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.BtnBasla);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnBasla;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button BtnTahminEt;
+        private System.Windows.Forms.Button BtnKelimeAl;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ KelimeOyunuProjesi/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace KelimeOyunuProjesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string kelime;
+        int uzunluk;
+        string[] harfler;
+
+      
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            kelime = "Mert Ağralı";
+            uzunluk = kelime.Length;
+            harfler = new string[uzunluk];
+            
+        }
+        public void LabelGetir()
+        {
+            label1.Text = " ";
+            for (int i = 0; i <= uzunluk; i++)
+            {
+                label1.Text += " _ ";
+            }
+        }
+
+        private void BtnBasla_Click(object sender, EventArgs e)
+        {
+
+            LabelGetir();
+
+           
+
+
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void BtnTahminEt_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
